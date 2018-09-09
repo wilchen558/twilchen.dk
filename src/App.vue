9eff350d9ce80fdf56cf0d77aba3baaffdd0506e
Tobias Wilchen-Pedersen <tobias@wilchen.dk>
2018-09-09 19:16:18 +0200

    Add vue-meta and add meta tags

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,19 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  metaInfo: {
+    title: 'Welcome',
+    titleTemplate: '%s | Comming soon',
+    meta: [
+      { name: 'description', content: 'Playground for Tobias Wilchen - Web developer based in Copenhagen' },
+    ]
+  }
+}
+</script>
+
+
 <style lang="scss">
 </style>
